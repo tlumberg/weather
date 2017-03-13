3bc4f892645a74edd6327e6cda8be3f9ea439b81
tlumberg <timlumberg@hotmail.com>
2017-03-13 09:40:40 -0700

    added base styles/html/scripts

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,24 @@
 // YOUR SCRIPTS GO HERE
+
+// https://cdnjs.cloudflare.com/ajax/libs/jquery.simpleWeather/3.1.0/jquery.simpleWeather.min.js
+
+$.simpleWeather({
+  location: 99205,
+  unit: 'f',
+  success: function(weather){
+      
+      // Weather Check
+    
+      console.log(weather);
+    
+      // Display 
+    $('.city').text(weather.city);
+    $('.state').text(weather.region);
+    $('.temp').text(weather.temp);
+    $('.thumb img').attr('src', weather.image);
+      
+    },
+  error:  function(error){
+      console.log('No Weather Found'); 
+    }
+});
