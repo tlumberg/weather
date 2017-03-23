7f2f96225e3211a95da4ae54d42f1606c9d27384
tlumberg <timlumberg@hotmail.com>
2017-03-22 21:18:49 -0700

    tried to get to work again for location

--- js/scripts.js
+++ js/scripts.js
@@ -10,97 +10,7 @@ $('#close').sidr({
 
 //WEATHER
 
-//Location
-if ('geolocation' in navigator) {
 
-   $('#homescreen').show(); 
-
-} else {
-  
-  $('#homescreen').hide();
-  $('#homescreen').prepend('<p>Geolocation Not Supported</p>');
-
-}
-
-// On Click, Get Geolocation, Call Weather Function
-$('#homescreen').click( function() {
-      
-    //load weather using your lat/long coordinates
-    navigator.geolocation.getCurrentPosition(function(position) {
-      
-      // Check lat/long coordinates
-      var lat = position.coords.latitude;
-      var long = position.coords.longitude;
-      
-      console.log(lat, long);
-      
-      // Send to SimpleWeather
-      getWeather( lat + ',' + long ); 
-    });
-   
-});
-
-// Get Weather
-var getWeather = function(location) {
-  
-    $.simpleWeather({
-    location: location,
-    unit: 'f',
-    success: function(weather) {
-      
-      // Entire weather object
-      console.log(weather);
-      
-      // Display Data
-      $('#homescreen .temp').text(weather.temp);
-      $('#homescreen .city').text(weather.city);
-      $('#homescreen i').addClass('icon-'+ weather.code);
-      $('#homescreen .high').text(weather.high);
-      $('#homescreen .low').text(weather.low);
-        
-        
-        // Get Condition Code
-      console.log(weather.code);
-      
-      if ( weather.code >= 0 && weather.code <= 12 ) {
-         $('#homescreen').addClass('storm');   
-      }
-      
-      if ( weather.code >= 13 && weather.code <= 18 ) {
-         $('#homescreen').addClass('snow');   
-      }
-      
-      if ( weather.code >= 19 && weather.code <= 22 ) {
-         $('#homescreen').addClass('haze');   
-      }
-      
-      if ( weather.code >= 23 && weather.code <= 24 ) {
-         $('#homescreen').addClass('wind');   
-      }
-      
-      if ( weather.code >= 25 && weather.code <= 30 ) {
-         $('#homescreen').addClass('cloud');   
-      }
-      
-      
-      if ( weather.code >= 31 && weather.code <= 36 ) {
-         $('#homescreen').addClass('sunny');   
-      }
-      
-      
-      if ( weather.code >= 37 && weather.code <= 47 ) {
-         $('#homescreen').addClass('storm');   
-      }
-
-    },
-    error: function(error) {
-      // Show if weather cannot be retreived
-      console.log('You fucked up, look outside.');
-    }
-  
-  });
-  
-};
 
 // Spokane
 $.simpleWeather({
@@ -215,4 +125,94 @@ $.simpleWeather({
   });
 
 
+//Location
+if ('geolocation' in navigator) {
 
+   $('#me').show(); 
+
+} else {
+  
+  $('#me').hide();
+  $('#me').prepend('<p>Geolocation Not Supported</p>');
+
+}
+
+// On Click, Get Geolocation, Call Weather Function
+$('#me').click( function() {
+      
+    //load weather using your lat/long coordinates
+    navigator.geolocation.getCurrentPosition(function(position) {
+      
+      // Check lat/long coordinates
+      var lat = position.coords.latitude;
+      var long = position.coords.longitude;
+      
+      console.log(lat, long);
+      
+      // Send to SimpleWeather
+      getWeather( lat + ',' + long ); 
+    });
+   
+});
+
+// Get Weather
+var getWeather = function(location) {
+  
+    $.simpleWeather({
+    location: location,
+    unit: 'f',
+    success: function(weather) {
+      
+      // Entire weather object
+      console.log(weather);
+      
+      // Display Data
+      $('#me .temp').text(weather.temp);
+      $('#me .city').text(weather.city);
+      $('#me i').addClass('icon-'+ weather.code);
+      $('#me .high').text(weather.high);
+      $('#me .low').text(weather.low);
+        
+        
+        // Get Condition Code
+      console.log(weather.code);
+      
+      if ( weather.code >= 0 && weather.code <= 12 ) {
+         $('#me').addClass('storm');   
+      }
+      
+      if ( weather.code >= 13 && weather.code <= 18 ) {
+         $('#me').addClass('snow');   
+      }
+      
+      if ( weather.code >= 19 && weather.code <= 22 ) {
+         $('#me').addClass('haze');   
+      }
+      
+      if ( weather.code >= 23 && weather.code <= 24 ) {
+         $('#me').addClass('wind');   
+      }
+      
+      if ( weather.code >= 25 && weather.code <= 30 ) {
+         $('#me').addClass('cloud');   
+      }
+      
+      
+      if ( weather.code >= 31 && weather.code <= 36 ) {
+         $('#me').addClass('sunny');   
+      }
+      
+      
+      if ( weather.code >= 37 && weather.code <= 47 ) {
+         $('#me').addClass('storm');   
+      }
+
+    },
+    error: function(error) {
+      // Show if weather cannot be retreived
+      console.log('You fucked up, look outside.');
+    }
+  
+  });
+  
+};
